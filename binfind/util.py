def294a4f124c0c6693a3e77341eee48df53ede4
Jayson Fong <fong.jayson@gmail.com>
2025-09-29 20:43:50 -0400

    file usage optimizations

--- binfind/util.py
+++ binfind/util.py
@@ -3,10 +3,11 @@ from typing import BinaryIO
 
 
 def space(obj: BinaryIO, offset: int, length: int, chunk_size: int = 1024 * 1024):
-    # Extend the file
     obj.seek(0, os.SEEK_END)
     end_position: int = obj.tell()
-    obj.truncate(end_position + length)
+
+    if offset == end_position:
+        return
 
     # Shift the data
     for pos in range(end_position, offset, -chunk_size):
