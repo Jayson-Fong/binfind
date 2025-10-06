5e05edff490653e63de7bf14e09d045af0431b81
Jayson Fong <fong.jayson@gmail.com>
2025-10-05 23:56:44 -0400

    Fix insert indexes and update README

--- README.md
+++ README.md
@@ -10,5 +10,7 @@ maintaining human readability. This package is **not** meant to enable access to
 
 This program is currently **not** a good candidate if you need to insert many entries at once as the program will 
 repeatedly perform disk I/O for each entry. This will likely be addressed in a future release for _small datasets that 
-can fit in memory_. At this time, you can expect that inserting many entries individually on a modern CPU may spend over
-70% of the time merely shifting data to insert in the middle of a file.
+can fit in memory_.
+
+Usage of this package heavily relies on heuristics to enable efficient I/O at scale, such as information about the size
+of entries and rough offset ranges of where a desired entry is or should be.
--- binfind/insert.py
+++ binfind/insert.py
@@ -17,9 +17,27 @@ def insert_fixed_key_entry(
         start_index: int = 0,
         end_index: int | None = None,
 ):
-    # This method assumes that the key is of a fixed
-    # length and all values already existing conform
-    # to this standard.
+    """
+    Insert an entry into a buffer.
+
+    This function assumes that keys are of a fixed length and
+    all values existing within the table conform to this standard.
+
+    Args:
+        obj: Buffer capable of read and write operations.
+        key: Key to insert.
+        value: Value to insert.
+        minimum_entry_size: Minimum length of entries in bytes, used as a heuristic.
+        maximum_entry_size: Maximum length of entries in bytes, used as a heuristic.
+        delimiter: Delimiter between entries.
+        chunk_size: Number of bytes to shift at a time.
+        start_index: Offset to start searching at.
+        end_index: Offset to stop searching at.
+
+    Returns:
+
+    """
+
     if minimum_entry_size is None or minimum_entry_size < len(key):
         minimum_entry_size: int = len(key)
 
@@ -44,7 +62,7 @@ def insert_fixed_key_entry(
         obj.seek(start_index, os.SEEK_SET)
         obj.write(delimiter + key + value)
 
-        return start_index, start_index + len(key) + len(value) + len(delimiter)
+        return start_index + len(delimiter), start_index + len(key) + len(value) + len(delimiter)
 
     while start_index < end_index - len(delimiter):
         search_value, search_start, search_end = search.get_entry_at(
@@ -67,9 +85,9 @@ def insert_fixed_key_entry(
         binfind.util.space(obj, offset=0, length=len(key) + len(value) + len(delimiter), chunk_size=chunk_size)
         obj.seek(start_index, os.SEEK_SET)
         obj.write(key + value + delimiter)
-        return start_index, len(key) + len(value)
+        return start_index, start_index + len(key) + len(value)
 
     binfind.util.space(obj, offset=start_index, length=len(key) + len(value) + len(delimiter), chunk_size=chunk_size)
     obj.seek(start_index, os.SEEK_SET)
     obj.write(delimiter + key + value)
-    return start_index, len(key) + len(value)
+    return start_index + len(delimiter), start_index + len(key) + len(value) + len(delimiter)
